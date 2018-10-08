078fe5bbf56d4cd94b19207c6adb41d897d0d174
varnit15112 <varnit15112@iiitd.ac.in>
2018-10-08 22:38:13 +0530

    python3 issues resolved

--- mainControl.py
+++ mainControl.py
@@ -13,7 +13,6 @@ from queue import Queue
 
 
 #Music Init
-
 Happy = {59+12,48+12,50+12,53+12,55+12,59+12,60+12, 60+12,58+12,57+12,53+12,52+12,50+12,55+12,53+12,52+12,50+12,52+12,57+12,48+12}
 Serious = {48+12,50+12,51+12,53+12,55+12,57+12,58+12,60+12,60+12,58+12,57+12,55+12,53+12,51+12,50+12,48+12}
 Angry = {48+12,49+12,54+12,59+12,60+12,60+12,59+12,56+12,55+12,54+12,52+12,49+12,48+12}
@@ -21,7 +20,28 @@ Gloomy = {48+12,50+12,53+12,55+12,56+12,58+12,60+12,58+12,56+12,55+12,53+12,51+1
 
 volumePrev = 0
 q = Queue(4)
+#End Music Init
 
+
+#Speech Init
+r = sr.Recognizer()
+mic = sr.Microphone()
+sr.Microphone.list_microphone_names()
+#End Speech Init
+
+
+#SSH Command Init
+SSH_ADDRESS = "192.168.2.22"
+SSH_USERNAME = "pi"
+SSH_PASSWORD = "raspberrypi"
+SSH_COMMAND_BASE = "cd Desktop/jamoora; python moveTest.py "
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_stdin = ssh_stdout = ssh_stderr = None
+#End SSH Command Init
+
+
+#Music Functions
 def is_a_in_x(A, X):
   for i in range(len(X) - len(A) + 1):
     if A == X[i:i+len(A)]: return True
@@ -51,48 +71,11 @@ def print_volume_handler(unused_addr, args, volume):
       print("Angry")
     if(which[3]):
       print("Gloomy")
-#End Music Init
-
-
-#Music Server Setup
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--ip", default="127.0.0.1", help="The ip of the OSC server")
-parser.add_argument("--port", type=int, default=8000, help="The port the OSC server is listening on")
-args = parser.parse_args()
-
-dispatcher = dispatcher.Dispatcher()
-dispatcher.map("/pitch", print_volume_handler, "Pitch")
-server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
-print("Serving on {}".format(server.server_address))
-server.serve_forever()
-
-#End Server
+#End Music Functions
 
 
 
-
-#Speech Init
-r = sr.Recognizer()
-mic = sr.Microphone()
-sr.Microphone.list_microphone_names()
-#End Speech Init
-
-
-#SSH Command Init
-SSH_ADDRESS = "192.168.2.22"
-SSH_USERNAME = "pi"
-SSH_PASSWORD = "raspberrypi"
-SSH_COMMAND_BASE = "cd Desktop/jamoora; python moveTest.py "
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_stdin = ssh_stdout = ssh_stderr = None
-
-#End SSH Command Init
-
-
-
-#execute
+#Speech Functions
 def executeCommand(SSH_COMMAND):
 
 	try:
@@ -107,7 +90,6 @@ def executeCommand(SSH_COMMAND):
 	    sys.stderr.write(ssh_stderr.read())
 
 
-#translate
 def express(audio1):
 	try:
 		
@@ -116,7 +98,7 @@ def express(audio1):
 		for i in command["alternative"]:			
 			finalComm += " " + i["transcript"]
 
-		print finalComm
+		print (finalComm)
 
 		#SSH_COMMAND = SSH_COMMAND_BASE + "PARAMS"
 
@@ -126,14 +108,37 @@ def express(audio1):
 		pass
 
 
+def continuousRec():
+	while(True):
+		with mic as source:
+			audio = r.record(source, duration=3) #listen
+			#print(" ")
+			#print(r.recognize_google(audio))
+			thread = Thread(target = express, args=(audio,))
+			thread.start()
 
-while(True):
-	with mic as source:
-		audio = r.record(source, duration=3) #listen
-		#print(" ")
-		#print(r.recognize_google(audio))
-		thread = Thread(target = express, args=(audio,))
-		thread.start()
+#End Speech Functions
+
+
+threadSpeech = Thread(target = continuousRec, args=(,))
+threadSpeech.start()
+
+
+#Music Server Setup
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="127.0.0.1", help="The ip of the OSC server")
+parser.add_argument("--port", type=int, default=8000, help="The port the OSC server is listening on")
+args = parser.parse_args()
+
+dispatcher = dispatcher.Dispatcher()
+dispatcher.map("/pitch", print_volume_handler, "Pitch")
+server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+print("Serving on {}".format(server.server_address))
+server.serve_forever()
+#End Server
+
+
+print ("lololololol")
 
 #print(r.recognize_google(audio))
 
